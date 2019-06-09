7820e1427dca8df66ce9ad217fbd9c4398b24cb8
Kirill Klebanov <kirillklebanov@Kirills-MacBook-Pro.local>
2019-06-09 07:36:20 +0300

    Add login request and parse a response

--- ios-client/ios-client/ViewController.swift
+++ ios-client/ios-client/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func loginTap(_ sender: Any) {
+        login(loginTextField.text ?? "", password: passwordTextField.text ?? "")
+    }
+}
+
+extension ViewController {
+    
+    func login(_ login: String, password: String) {
+       
+        let url = URL(string: "http://edu-server.herokuapp.com/api/login")!
+        var request = URLRequest(url: url)
+        request.setValue("text/plain; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let parameters: [String: Any] = [
+            "login": "13",
+            "password": "Jack & Jill"
+        ]
+        let jsonData = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        request.httpBody = jsonData
+        
+        Alamofire.request(request).responseJSON {[weak self] response in
+//            guard response.result.isSuccess else {
+//                print("Ошибка при запросе данных \(String(describing: response.result.error))")
+//                return
+//            }
+            print(response.value)
+            if false {
+                self?.performSegue(withIdentifier: "openPoll", sender: self)
+            } else {
+                self?.performSegue(withIdentifier: "openMain", sender: self)
+            }
+        }
+    }
 }
 
